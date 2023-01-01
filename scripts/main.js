a1013c0cf13c8f854af8724f82bf93a687fee78f
wilgnerjr <76569878+wilgnerjr@users.noreply.github.com>
2023-01-01 16:22:54 -0400

    atualização

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var img = document.getElementById('imgHeader');
 var btnCurriculo = document.getElementById('btnCurriculo');
 var container = document.getElementById('container');
-
+alert('Olá Seja Bem Vindo!\n \nPassando aqui só para avisar que este portifólio está em constantes \natualizações volte sempre e confira as novidades');
 
 btnCurriculo.addEventListener('mouseover', function(){
     img.classList.toggle('efeito');
@@ -13,3 +13,4 @@ btnCurriculo.addEventListener('mouseout', () => {
     img.classList.toggle('efeito');
     // btnCurriculo.classList.toggle('efeito');
 });
+
